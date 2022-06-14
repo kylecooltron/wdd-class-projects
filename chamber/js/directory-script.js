35d84bd61bc2cc729acfedc6735e73f00abd971b
kylecooltron <kylejcoulon@gmail.com>
2022-06-14 10:54:10 -0600

    fixed discover page

---
+++ chamber/js/directory-script.js
@@ -0,0 +1,48 @@
+
+const requestURL = 'json/data.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const businesses = jsonObject['businesses'];
+    businesses.forEach(displayProphets);
+  });
+
+
+  function displayProphets(business) {
+    // Create elements to add to the document
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let p1 = document.createElement('p');
+    let p2 = document.createElement('p');
+    let iconImg = document.createElement('img');
+  
+    // Change the textContent property of the h2 element to contain the prophet's full name
+    h2.textContent = `${business.name}`;
+  
+    // Change the textContent property of the p1 element to contain the business address
+    p1.textContent = business.address;
+
+    // Change the textContent property of the p2 element to contain the business phone
+    p2.textContent = business.phone;
+
+    // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
+    iconImg.setAttribute('src', business.imageurl);
+    iconImg.setAttribute('alt', `Icon image for ${business.name}`);
+    iconImg.setAttribute('loading', 'lazy');
+  
+    // Add/append the section(card) with the h2 element
+    card.appendChild(h2);
+    card.appendChild(p1);
+    card.appendChild(p2);
+    card.appendChild(iconImg);
+  
+    // Add/append the existing HTML div with the '.directory-grid' class with the section(card)
+    document.querySelector('.directory-grid').appendChild(card);
+  }
+
+
+  
