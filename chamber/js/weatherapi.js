2a8725e9b03a8d16e1c0ad5d8bf4b43b14a31704
kylecooltron <kylejcoulon@gmail.com>
2022-06-21 15:32:09 -0600

    tempcolor update

--- chamber/js/weatherapi.js
+++ chamber/js/weatherapi.js
@@ -3,6 +3,8 @@ import update_windchil from './windchill.js';
 
 // select HTML elements to edit
 const currentTemp = document.querySelector('#current-temp');
+const currentTempText = document.querySelector('.weather-temp');
+
 const currentHumi = document.querySelector('#current-humidity');
 const currentWind = document.querySelector('#current-wind');
 
@@ -12,13 +14,42 @@ const captionDesc = document.querySelector('figcaption');
 //API URL with arguments  
 const APIurl = "//api.openweathermap.org/data/2.5/weather?id=5562087&appid=7c894b69ae7ae90c1d0eac7949ebdf76&units=imperial"
 
+
+
+function interpolate_color(startcolor, endcolor, percent){
+  let rgb = [];
+  rgb.push(Math.round(startcolor[0] + (endcolor[0] - startcolor[0]) * percent));
+  rgb.push(Math.round(startcolor[1] + (endcolor[1] - startcolor[1]) * percent));
+  rgb.push(Math.round(startcolor[2] + (endcolor[2] - startcolor[2]) * percent));
+  return rgb;
+}
+
+function get_temp_color(temperature){
+  let tempcolor = [0,0,0];
+  // adjust temperature to be a useful decimal
+  temperature = (temperature-29.8)*0.01;
+  if(temperature <= 0.5){   // blue to yellow
+  tempcolor = interpolate_color([0,90,200],[255,255,0],temperature*2);
+  }else{                    // yellow to red
+  tempcolor = interpolate_color([255,255,0],[255,0,0],(temperature*2)-0.5);
+  }
+  //return as rgb(0,0,0) string
+  return "rgb(" + tempcolor[0] + "," + tempcolor[1] + "," + tempcolor[2] + ")";
+}
+
+
 fetch(APIurl)
   .then((response) => response.json())
   .then((weatherinfo) => {
 
     console.log(weatherinfo); // this is temporary for development only
 
-    currentTemp.innerHTML = `<strong>${weatherinfo.main.temp.toFixed(0)}</strong>`;
+    //set temperature and temperature text color
+    let temp = weatherinfo.main.temp.toFixed(0);
+    currentTemp.innerHTML = `<strong>${temp}</strong>`;
+    currentTempText.style.color = get_temp_color(temp);
+
+
     currentHumi.innerHTML = `<strong>${weatherinfo.main.humidity.toFixed(0)}</strong>`;
     currentWind.innerHTML = `<strong>${weatherinfo.wind.speed.toFixed(0)}</strong>`;
     
